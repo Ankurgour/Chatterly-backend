2312a98a5b8c7f0c08e156899a31c1dbce754818
Ankur Gour <ankurgour2003@gmail.com>
2024-06-23 14:05:06 +0530

    cors origin changed

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","*"],
     credentials: true,
   })
 );
